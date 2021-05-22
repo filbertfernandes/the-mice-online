46f714d53787c9f3696156e5a583ce361b05725e
filbertfernandes <filbertfernandes.bot@gmail.com>
2021-05-22 14:17:51 +0700

    modified

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -7,7 +7,7 @@
           <h2 class="card-title text-center">THE MICE</h2>
           <div class="box">
             <form action="<?= base_url('auth') ?>" method="post">
-              <input type="text" class="mt-3" id="nickname" name="nickname" placeholder="Enter your nickname" value="<?= set_value('nickname') ?>">
+              <input type="text" class="mt-3" id="nickname" name="nickname" placeholder="Enter your nickname" autocomplete="off" value="<?= set_value('nickname') ?>">
               <?= form_error('nickname', '<small style="color: red;font-weight: bold;" class="pl-1">', '</small>'); ?>
               <button type="submit" class="mt-3 mb-4" >Play!</button>
             </form>
